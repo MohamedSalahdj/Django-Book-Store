e93858a12ec316d4445445d48917f939aa554390
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-03-12 23:15:31 +0200

    added category , book model without migrate it waiting author author model

--- book/models.py
+++ book/models.py
@@ -1,11 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.text import slugify
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(null=True, blank=True)
 
-class book(models.Model):
-    pass
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
+book_language = (
+    ('ar', 'Arabic'),
+    ('en', 'English'),
+    ('fr', 'French')
+)
 
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    language = models.CharField(max_length=2, choices=book_language)
+    no_of_page = models.IntegerField()
+    year_of_publication = models.DateField()
+    total_number_of_book = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='book_category')
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='book_author')
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='publisher_book')
+    slug  = models.SlugField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Book, self).save(*args, **kwargs)
