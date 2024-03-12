eac341b615db2700f9c377e6932f50aef3ee634b
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-03-12 16:39:31 +0200

    create empty  category , book model

--- book/models.py
+++ book/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# class Category(models.Model):
-#     pass
+class Category(models.Model):
+    pass
 
-# class book(models.Model):
-#     pass
+class book(models.Model):
+    pass
 
 
