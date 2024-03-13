1b79f767a49f293fc5ac546c2f8e891fe16f040b
mostafa_hassan <mostafahassan5000@gmail.com>
2024-03-13 14:43:56 +0200

    after merge

--- account/models.py
+++ account/models.py
@@ -27,8 +27,8 @@ class Author(models.Model):
 #^ ----------------------------------------------  Publisher model -----------------------------------------
 
 class Publisher(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
     certificate = models.FileField(upload_to='certificate/',blank=True,null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
