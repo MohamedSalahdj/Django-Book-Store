8b42c39bc286349bce990afbc1a39287e26e936f
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-03-17 10:50:47 +0200

    Adding Paggination To The PublisherOrderList

--- order/views.py
+++ order/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Orderlist
 from .serializers import OrderSerializer
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 
 
@@ -10,5 +11,7 @@ from .serializers import OrderSerializer
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Orderlist.objects.all()
     serializer_class = OrderSerializer
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 7
 
 
