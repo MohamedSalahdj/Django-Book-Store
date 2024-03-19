f3ee0c1b7e7a14eb401239dca5c082c349ee227a
Shahd Ahmed <153300892+shahdahmedabdelraoufmohamed@users.noreply.github.com>
2024-03-19 06:39:04 +0200

    Update views.py

--- carts/views.py
+++ carts/views.py
@@ -3,14 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-from api.carts.models import Cart, CartItem
-from api.products.models import Product
-from .serializers import CartSerializer, CartItemSerializer
-from rest_framework import generics, status
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from api.orders.models import Order, OrderItem
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import NotAcceptable, ValidationError
 
 
