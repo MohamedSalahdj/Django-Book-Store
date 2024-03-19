e4c60b8c2865af432632b90a7eacdb1bf5a0f6b4
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-03-19 07:34:14 +0200

    Revert "fix data base conflit"
    This reverts commit 826e560c8da23da1e1df9e9346f9a7dafcfe54ed, reversing
    changes made to 7e9d3be6047b07d34468ba220a66c27d3be0202a.

--- book/models.py
+++ book/models.py
@@ -14,38 +14,20 @@ class Category(models.Model):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
-    @classmethod
-    def create_categ(cls):
-        cls.objects.create(name="Fiction")
-        cls.objects.create(name="Nonfiction")
-        cls.objects.create(name="Action and adventure")
-        cls.objects.create(name="Art/architecture")
-        cls.objects.create(name="Autobiography")
-        cls.objects.create(name="Anthology")
-        cls.objects.create(name="Biography")
-        cls.objects.create(name="Classic")
-        cls.objects.create(name="Cookbook")
-        cls.objects.create(name="Dictionary")
-        cls.objects.create(name="Guide")
-        cls.objects.create(name="Humor")
-        cls.objects.create(name="Horror")
-        cls.objects.create(name="Journal")
-
 
 book_language = (
     ('Arabic', 'Arabic'),
     ('English', 'English'),
+    ('French', 'French')
 )
 
 class Book(models.Model):
     name = models.CharField(max_length=255)
-    ISBN = models.CharField(max_length=255, unique=True)
-    front_img = models.ImageField(upload_to='book/')
-    back_img = models.ImageField(upload_to='book/')
+    img = models.ImageField(upload_to='book/')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     language = models.CharField(max_length=7, choices=book_language)
-    no_of_page = models.IntegerField(null=True, blank=True)
+    no_of_page = models.IntegerField()
     year_of_publication = models.DateField()
     total_number_of_book = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='book_category')
