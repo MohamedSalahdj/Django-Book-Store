d124d0064deb86b1c61bfcb421bfeef9628c8c29
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-03-19 20:57:23 +0200

    add Update and Delete To Order Model

--- order/views.py
+++ order/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view,permission_classes
 from .serializers import OrderItemsSerializer,OrderSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated 
-from rest_framework import generics, status
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -20,12 +20,12 @@ def add_order(request):
     if order_items and len(order_items) == 0:
        return Response({'error': 'No order recieved'},status=status.HTTP_400_BAD_REQUEST)
     else:
-        total_amount = sum( item['price']* item['quantity'] for item in order_items)
+        # total_amount = sum( item['price']* item['quantity'] for item in order_items)
         order = Order.objects.create(
             user = user,
             status= data['status'],
             quantity = data['quantity'],
-            total_amount = total_amount,
+            # total_amount = total_amount,
         )
         for i in order_items:
             book = Book.objects.get(id=i['book'])
@@ -35,7 +35,7 @@ def add_order(request):
                 quantity = i['quantity'],
                 price = i['price']
             )
-            book.stock -= item.quantity
+            book.total_number_of_book -= item.quantity
             book.save()
         serializer = OrderSerializer(order,many=False)
         return Response(serializer.data)
@@ -47,6 +47,7 @@ def get_orders(request):
     serializer = OrderSerializer(orders,many=True)
     return Response({'orders':serializer.data})
 
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_order(request,pk):
@@ -57,8 +58,24 @@ def get_order(request,pk):
 
 
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated,IsAdminUser])
+def update_order(request,id):
+    order =get_object_or_404(Order, id=id)
+    order.status = request.data['status']
+    order.save()
+     
+    serializer = OrderSerializer(order,many=False)
+    return Response({'order':serializer.data})
 
 
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_order(request,pk):
+    order =get_object_or_404(Order, id=pk) 
+    order.delete()
+      
+    return Response({'details': "order is deleted"})
 
 
 
