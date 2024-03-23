af8094e3b1014215de525086128ca10ee61545b1
Ali Bayomy <alibayomy12@gmail.com>
2024-03-23 14:51:23 +0200

    adding jasmine, link the author to the publisher and adding an end point for authors per publisher

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
