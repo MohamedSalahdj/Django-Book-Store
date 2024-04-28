1de65211bc5ec4cc87e79de98666862cbda923a1
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-04-28 05:02:40 +0300

    chage date time feild

--- order/models.py
+++ order/models.py
@@ -22,6 +22,7 @@ order_status = (
 class Order(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
     ordered_date = models.DateField(auto_now_add=True)
+    ordered_time = models.DateTimeField(auto_now_add=True) 
     updated_date = models.DateField(auto_now_add=True)
 
     status = models.CharField(max_length=20, choices=order_status)
